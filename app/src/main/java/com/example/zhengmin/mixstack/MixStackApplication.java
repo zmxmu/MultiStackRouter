f58db4c017b05c002d3fb7071b131c1e60f7a19c
“zhengmin” <zhengmin1@syswin.com>
2019-02-25 18:07:48 +0800

    添加新module

--- app/src/main/java/com/example/zhengmin/mixstack/MixStackApplication.java
+++ app/src/main/java/com/example/zhengmin/mixstack/MixStackApplication.java
@@ -1,18 +1,8 @@
 package com.example.zhengmin.mixstack;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
-import android.text.TextUtils;
 
-import com.syswin.msgseal.mixstack.BaseFragmentActivity;
-import com.syswin.msgseal.mixstack.ActivityItem;
-import com.syswin.msgseal.mixstack.RouterItem;
-import com.syswin.msgseal.mixstack.RouterManager;
-
-import java.lang.ref.WeakReference;
-import java.util.Iterator;
-import java.util.Stack;
+import com.syswin.msgseal.mixstack.SwitchBackgroundCallbacks;
 
 /**
  * Created by zhengmin on 2019/2/21.
@@ -26,68 +16,4 @@ public class MixStackApplication extends Application {
         registerActivityLifecycleCallbacks(new SwitchBackgroundCallbacks());
     }
 
-
-    private class SwitchBackgroundCallbacks implements ActivityLifecycleCallbacks {
-
-        @Override
-        public void onActivityCreated(Activity activity, Bundle bundle) {
-            String routerPath;
-            Bundle paraBundle = activity.getIntent().getExtras();
-            if(paraBundle!=null){
-                routerPath=paraBundle.getString(RouterManager.BUNDLE_KEY_PATH);
-            }
-            else{
-                routerPath = activity.getClass().getName();
-            }
-            WeakReference<Activity> activityWeakReference = new WeakReference<Activity>(activity);
-            if(TextUtils.equals(routerPath,BaseFragmentActivity.class.getName())){
-                RouterManager.getInstance().getStack().push(
-                        new ActivityItem(RouterItem.ROUTER_TYPE_CONTAINER,activityWeakReference,routerPath));
-            }
-            else{
-                RouterManager.getInstance().getStack().push(
-                        new ActivityItem(RouterItem.ROUTER_TYPE_ACTIVITY,activityWeakReference,routerPath));
-            }
-        }
-
-        @Override
-        public void onActivityStarted(Activity activity) {
-
-        }
-
-        @Override
-        public void onActivityResumed(Activity activity) {
-
-        }
-
-        @Override
-        public void onActivityPaused(Activity activity) {
-
-        }
-
-        @Override
-        public void onActivityStopped(Activity activity) {
-
-        }
-
-        @Override
-        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onActivityDestroyed(Activity activity) {
-            Stack<RouterItem> stack = RouterManager.getInstance().getStack();
-            Iterator<RouterItem> itemIterator = stack.iterator();
-            while(itemIterator.hasNext()){
-                RouterItem currentItem = itemIterator.next();
-                if(currentItem instanceof ActivityItem){
-                    if(activity == (Activity)((ActivityItem) currentItem).getActivityWR().get()){
-                        itemIterator.remove();
-                        break;
-                    }
-                }
-            }
-        }
-    }
 }
